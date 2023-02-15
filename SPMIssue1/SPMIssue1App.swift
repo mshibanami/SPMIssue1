3f4b6f4bbd21699303ab10d8da0ff742ea9097f2
Manabu Nakazawa <mshibanami+git@gmail.com>
2023-02-15 16:32:16 +0900

    init

---
+++ SPMIssue1/SPMIssue1App.swift
@@ -0,0 +1,17 @@
+//
+//  SPMIssue1App.swift
+//  SPMIssue1
+//
+//  Created by Manabu Nakazawa on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct SPMIssue1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
